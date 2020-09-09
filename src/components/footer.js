4d1824a1503cb178f0dfe2f818f646758442cc9d
Miah <revolution5117@gmail.com>
2020-09-09 15:59:29 -0700

    Project complete

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+ const Footer = () => {
+    return(
+        <div className = 'sources'>
+            <p>Authors and editors: Robert Nemiroff (MTU) and Jerry Bonnell (UMCP)
+                NASA Official: Phillip Newman Specific rights apply.
+                NASA Web Privacy Policy and Important Notices
+                A service of: ASD at NASA / GSFC
+                and Michigan Tech. U.
+            </p>
+        </div>
+    )
+}
+export default Footer
---
+++ src/components/header.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+
+ const Header = () => {
+     
+return(
+    <div className = 'title'>
+        <h2>Astronomy Picture</h2>
+        <p>Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.
+        </p>
+    </div>
+)
+}
+
+export default Header
